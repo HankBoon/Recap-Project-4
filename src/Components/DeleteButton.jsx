d5a03d954e7fe5dd7f395cccb9a9faaf0da579e3
Hauke <hank.la.boon@gmail.com>
2024-11-25 16:14:00 +0100

    add deleteButton

--- src/Components/DeleteButton.jsx
+++ src/Components/DeleteButton.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import "./DeleteButton.css";
+
 export default function DeleteButton() {
-  return <button></button>;
+  const [buttonStatus, setButtonStatus] = useState(false);
+
+  //   function handleDeleteColor(){
+
+  //   }
+
+  return (
+    <div className="delete-button-container">
+      {!buttonStatus ? (
+        <button onClick={() => setButtonStatus(!buttonStatus)}>DELETE</button>
+      ) : (
+        <>
+          <p>really delete this color?</p>
+          <button onClick={() => setButtonStatus(!buttonStatus)}>CANCEL</button>
+          <button
+            onClick={() => {
+              setButtonStatus(!buttonStatus);
+            }}
+          >
+            DELETE
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
