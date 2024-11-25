15cffa6af1235ad639b3f6b7e99d80f299020674
Hauke <hank.la.boon@gmail.com>
2024-11-25 19:57:34 +0100

    add functions

--- src/Components/DeleteButton.jsx
+++ src/Components/DeleteButton.jsx
@@ -2,36 +2,46 @@ import { useState } from "react";
 import "./DeleteButton.css";
 
 export default function DeleteButton({ onDeleteColor, colorToDelete }) {
-  const [deleteButton, setDeleteButton] = useState(false);
+  const [deleteButton, setDeleteButton] = useState(true);
   const [editButton, setEditButton] = useState(true);
+
+  function DeleteButton() {
+    return (
+      <>
+        {" "}
+        <button onClick={() => setDeleteButton(!deleteButton)}>DELETE</button>
+        <button
+          onClick={() => {
+            null;
+          }}
+        >
+          EDIT
+        </button>
+      </>
+    );
+  }
+
+  function DeleteButtonExpanded() {
+    return (
+      <>
+        <p>really delete this color?</p>
+        <button onClick={() => setDeleteButton(!deleteButton)}>CANCEL</button>
+        <button
+          onClick={() => {
+            onDeleteColor(colorToDelete);
+            setDeleteButton(!deleteButton);
+          }}
+        >
+          DELETE
+        </button>
+      </>
+    );
+  }
 
   return (
     <div className="delete-button-container">
-      {!deleteButton ? (
-        <>
-          <button onClick={() => setDeleteButton(!deleteButton)}>DELETE</button>
-          <button
-            onClick={() => {
-              null;
-            }}
-          >
-            EDIT
-          </button>
-        </>
-      ) : (
-        <>
-          <p>really delete this color?</p>
-          <button onClick={() => setDeleteButton(!deleteButton)}>CANCEL</button>
-          <button
-            onClick={() => {
-              onDeleteColor(colorToDelete);
-              setDeleteButton(!deleteButton);
-            }}
-          >
-            DELETE
-          </button>
-        </>
-      )}
+      {/* if(!deleteButton){{ DeleteButton }} */}
+      {deleteButton ? <DeleteButton /> : <DeleteButtonExpanded />}
     </div>
   );
 }
