2440bb8fa45a5a76e24985236e7c5f27fefaca8c
Hauke <hank.la.boon@gmail.com>
2024-11-25 14:21:04 +0100

    add form with function

--- src/Components/ColorInput.jsx
+++ src/Components/ColorInput.jsx
@@ -1,25 +1,29 @@
 import { useState } from "react";
 
-export default function ColorInput({ labelValue, id }) {
+export default function ColorInput({ labelValue, id, className }) {
   const [colorValue, setColorValue] = useState("");
   return (
     <>
-      <label htmlFor={id}>{labelValue}</label>{" "}
-      <input
-        type="text"
-        id={id}
-        name={id}
-        placeholder="#rrggbb"
-        value={colorValue}
-        onChange={(event) => setColorValue(event.target.value)}
-      />
-      <input
-        type="color"
-        id={id}
-        name={id}
-        value={colorValue}
-        onChange={(event) => setColorValue(event.target.value)}
-      />
+      <label className={className} htmlFor={id}>
+        {labelValue}
+      </label>
+      <div className={className}>
+        <input
+          type="text"
+          id={id}
+          name={id}
+          placeholder="#rrggbb"
+          value={colorValue}
+          onChange={(event) => setColorValue(event.target.value)}
+        />
+        <input
+          type="color"
+          id={id}
+          name={id}
+          value={colorValue}
+          onChange={(event) => setColorValue(event.target.value)}
+        />
+      </div>
     </>
   );
 }
