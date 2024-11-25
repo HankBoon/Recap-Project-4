14ecae70e86429977810bdda53317b8323e011b8
Hauke <hank.la.boon@gmail.com>
2024-11-25 15:12:30 +0100

    add functionality to add colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,31 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 import ColorForm from "./Components/ColorForm";
+import { useState } from "react";
+import { uid } from "uid";
+
 // import ColorInput from "./Components/ColorInput";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleSubmitNewColor(newColor) {
+    setColors([
+      {
+        id: uid(),
+        role: newColor.role,
+        hex: newColor["hex-text-input"],
+        contrastText: newColor["contrast-text-input"],
+      },
+      ...colors,
+    ]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
-      <ColorForm />
-      {initialColors.map((color) => {
+      <ColorForm onSubmitNewColor={handleSubmitNewColor} />
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
--- src/Components/ColorForm.jsx
+++ src/Components/ColorForm.jsx
@@ -2,14 +2,16 @@ import "./ColorForm.css";
 import "../index.css";
 import ColorInput from "./ColorInput";
 
-export default function ColorForm() {
+export default function ColorForm({ onSubmitNewColor }) {
   function handleFormSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
     console.log(data);
+    onSubmitNewColor(data);
     event.target.reset();
   }
+
   return (
     <form className="color-form" onSubmit={handleFormSubmit}>
       <label htmlFor="role">Role</label>
