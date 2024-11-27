49edbda25742182f9b9fb1595641e3ac331831bd
Hauke <hank.la.boon@gmail.com>
2024-11-27 10:51:40 +0100

    update ContrastCheck

--- src/Components/ContrastCheck.jsx
+++ src/Components/ContrastCheck.jsx
@@ -1,18 +1,16 @@
-export default function ContrastCheck({ hex1, hex2 }) {
-  async function postFetch() {
-    const response = await fetch(
-      "https://www.aremycolorsaccessible.com/api/are-they",
-      {
-        method: "POST",
-        body: JSON.stringify({ colors: [hex1, hex2] }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const dataFromResponse = response.json();
-    console.log(dataFromResponse);
-  }
+export default async function ContrastCheck({ hex1, hex2 }) {
+  const response = await fetch(
+    "https://www.aremycolorsaccessible.com/api/are-they",
+    {
+      method: "POST",
+      body: JSON.stringify({ colors: [hex1, hex2] }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const dataFromResponse = response.json();
+  console.log(dataFromResponse);
 }
 
 // body needs 2 color values
